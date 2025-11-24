dba25028746cfcbf28b906ffddb8694adaac2a69
MatAviez05 <baezavilamateo@gmail.com>
2025-11-24 14:09:12 -0300

    conexion entre routepedidos y app

---
+++ backend/src/routes/pedidos.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import verifyTokenMiddleware from "../middlewares/verifyToken.middleware";
+
+export const routerPedido = Router()
+
+routerPedido.post
+routerPedido.get
+routerPedido.get
+routerPedido.put
+routerPedido.delete
+
+export default routerPedido
