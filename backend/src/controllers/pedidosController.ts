f32272ed30cd9e7fd1f03a5c9da32c6424628b36
MatAviez05 <baezavilamateo@gmail.com>
2025-11-24 14:14:54 -0300

    conexion entre pedido router y pedido controller

---
+++ backend/src/controllers/pedidosController.ts
@@ -0,0 +1,26 @@
+import { Pedido } from "../models/Pedido";
+
+export class PedidoController{
+    
+    public async getAllPedidos(){
+
+    }
+
+    public async getPedidoByCliente(){
+
+    }
+
+    public async addPedido(){
+
+    }
+
+    public updatePedido(){
+
+    }
+
+    public deletePedido(){
+
+    }
+}
+
+export default new PedidoController
--- backend/src/routes/pedidos.routes.ts
+++ backend/src/routes/pedidos.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
+import PedidosController from '../controllers/pedidosController';
 import verifyTokenMiddleware from "../middlewares/verifyToken.middleware";
 
 export const routerPedido = Router()
 
-routerPedido.post
-routerPedido.get
-routerPedido.get
-routerPedido.put
-routerPedido.delete
+routerPedido.post('/', PedidosController.addPedido)
+routerPedido.get('/', PedidosController.getAllPedidos)
+routerPedido.get('/mis-pedidos', PedidosController.getPedidoByCliente)
+routerPedido.put('/:id', PedidosController.updatePedido)
+routerPedido.delete('/:id', PedidosController.deletePedido)
 
 export default routerPedido
